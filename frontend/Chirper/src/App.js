66e364524872820076bb13e1ee0ce13736a89c68
Mic Doodle <mcdoodle@Mics-MBP.fios-router.home>
2020-04-17 11:22:30 -0400

    basic react spinup

---
+++ frontend/Chirper/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Feed from './components/Feed'
+
+function App() {
+  return (
+    <body>
+      App-Body
+      <Feed />
+    </body>
+  );
+}
+
+export default App;
---
+++ frontend/Chirper/src/components/Feed.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Users from './Users'
+
+function Feed() {
+    return (
+        <>
+            <br />
+            Everything should be called in here
+            <section>Display of getAll Chirps</section>
+            <Users />
+        </>
+    )
+}
+
+export default Feed
